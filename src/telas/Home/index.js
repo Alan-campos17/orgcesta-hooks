4381b22ebff5b13e977158acff75aa3bf83237ae
Alan-campos17 <149601081+Alan-campos17@users.noreply.github.com>
2025-04-04 14:42:37 -0300

    Add files via upload

---
+++ src/telas/Home/index.js
@@ -0,0 +1,10 @@
+import react from "react";
+import Topo from './componentes/Topo';
+import Produtores from "./componentes/produtores";
+
+export default function Home(){
+    return<>
+   <Topo />
+   <Produtores/>
+   </>
+}
