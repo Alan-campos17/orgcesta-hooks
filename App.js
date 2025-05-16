cf432d991a91b67f24de4eb6a71dd18d7974b85b
Alan-campos17 <149601081+Alan-campos17@users.noreply.github.com>
2025-05-16 14:52:28 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { StatusBar, SafeAreaView } from 'react-native';
+import AppRotas from './src/rotas/AppRotas';
 
-import Home from './src/telas/Home';
-
-function App() {
-  return <SafeAreaView style={estilos.tela}>
-      <Home />
-    </SafeAreaView>
+export default function App() {
+ 
+  return <SafeAreaView style={{ flex: 1 }}>
+    <StatusBar />
+      <AppRotas />
+  </SafeAreaView>
 }
-
-const estilos = StyleSheet.create({
-  tela: {
-    flex: 1,
-  }
-})
-
-export default App;
